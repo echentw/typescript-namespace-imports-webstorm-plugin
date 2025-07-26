5de0e029f41c95c4181892e70128d832add01158
GitHub Action <action@github.com>
2025-07-26 21:54:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/echentw/typescriptnamespaceimportswebstormplugin/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/echentw/typescriptnamespaceimportswebstormplugin/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.echentw.typescriptnamespaceimportswebstormplugin.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
