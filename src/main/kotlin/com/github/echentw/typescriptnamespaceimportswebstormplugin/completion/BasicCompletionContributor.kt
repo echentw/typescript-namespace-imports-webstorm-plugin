a2ba36d2250765d7178aceca00ba9f93e70e8119
Eric Chen <ericchen098@gmail.com>
2025-07-26 17:20:28 -0700

    wip

--- src/main/kotlin/com/github/echentw/typescriptnamespaceimportswebstormplugin/completion/BasicCompletionContributor.kt
+++ src/main/kotlin/com/github/echentw/typescriptnamespaceimportswebstormplugin/completion/BasicCompletionContributor.kt
@@ -18,17 +18,10 @@ class BasicCompletionContributor : CompletionContributor() {
                     context: ProcessingContext,
                     result: CompletionResultSet
                 ) {
-                    // Always add a test completion (for debugging)
-                    result.addElement(
-                        LookupElementBuilder.create("DEBUG_TEST")
-                            .withPresentableText("DEBUG_TEST")
-                            .withTailText(" (Plugin is working!)")
-                    )
-                    
                     // Get the prefix being typed
                     val prefix = result.prefixMatcher.prefix
                     
-                    // Check if the prefix contains "asdf"
+                    // Check if the prefix is "asdf"
                     if (prefix.contains("asdf")) {
                         result.addElement(
                             LookupElementBuilder.create("asdf!")
