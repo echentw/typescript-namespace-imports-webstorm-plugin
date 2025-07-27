5fc4e1127938c29d55fe7abc7e2dcd98c403c374
Eric Chen <ericchen098@gmail.com>
2025-07-26 17:13:29 -0700

    works

--- src/main/kotlin/com/github/echentw/typescriptnamespaceimportswebstormplugin/completion/BasicCompletionContributor.kt
+++ src/main/kotlin/com/github/echentw/typescriptnamespaceimportswebstormplugin/completion/BasicCompletionContributor.kt
@@ -18,36 +18,23 @@ class BasicCompletionContributor : CompletionContributor() {
                     context: ProcessingContext,
                     result: CompletionResultSet
                 ) {
-                    // Get the text before the cursor
-                    val elementAtCaret = parameters.position
-                    val text = elementAtCaret.text
+                    // Always add a test completion (for debugging)
+                    result.addElement(
+                        LookupElementBuilder.create("DEBUG_TEST")
+                            .withPresentableText("DEBUG_TEST")
+                            .withTailText(" (Plugin is working!)")
+                    )
                     
-                    // Check if the current text contains "asdf"
-                    if (text.contains("asdf")) {
-                        // Add a completion suggestion
-                        val lookupElement = LookupElementBuilder.create("asdf!")
-                            .withPresentableText("asdf!")
-                            .withTailText(" (Basic completion test)")
-                            .withInsertHandler { context, _ ->
-                                // Replace "asdf" with "asdf!"
-                                val document = context.document
-                                val startOffset = context.startOffset
-                                val currentText = document.getText()
-                                val lineStart = currentText.lastIndexOf('\n', startOffset) + 1
-                                val lineEnd = currentText.indexOf('\n', startOffset).let { 
-                                    if (it == -1) currentText.length else it 
-                                }
-                                val lineText = currentText.substring(lineStart, lineEnd)
-                                
-                                val asdfIndex = lineText.indexOf("asdf")
-                                if (asdfIndex != -1) {
-                                    val replaceStart = lineStart + asdfIndex
-                                    val replaceEnd = replaceStart + 4 // length of "asdf"
-                                    document.replaceString(replaceStart, replaceEnd, "asdf!")
-                                }
-                            }
-                        
-                        result.addElement(lookupElement)
+                    // Get the prefix being typed
+                    val prefix = result.prefixMatcher.prefix
+                    
+                    // Check if the prefix contains "asdf"
+                    if (prefix.contains("asdf")) {
+                        result.addElement(
+                            LookupElementBuilder.create("asdf!")
+                                .withPresentableText("asdf!")
+                                .withTailText(" (Basic completion test)")
+                        )
                     }
                 }
             }
