00554d76e23feae8af45aa163ef44994e7056a95
Eric Chen <ericchen098@gmail.com>
2025-08-25 22:56:29 -0700

    do not suggest an already-imported module

--- src/main/kotlin/com/github/echentw/typescriptnamespaceimportswebstormplugin/completion/ExtensionCompletionContributor.kt
+++ src/main/kotlin/com/github/echentw/typescriptnamespaceimportswebstormplugin/completion/ExtensionCompletionContributor.kt
@@ -1,6 +1,5 @@
 package com.github.echentw.typescriptnamespaceimportswebstormplugin.completion
 
-import com.github.echentw.typescriptnamespaceimportswebstormplugin.services.ModuleForCompletion
 import com.github.echentw.typescriptnamespaceimportswebstormplugin.services.ExtensionServiceImpl
 import com.intellij.codeInsight.completion.*
 import com.intellij.codeInsight.lookup.LookupElementBuilder
@@ -12,40 +11,49 @@ import com.intellij.psi.PsiDocumentManager
 
 class ExtensionCompletionContributor : CompletionContributor() {
     init {
-        extend(CompletionType.BASIC, PlatformPatterns.psiElement(), object : CompletionProvider<CompletionParameters>() {
-            override fun addCompletions(
-                parameters: CompletionParameters,
-                context: ProcessingContext,
-                result: CompletionResultSet
-            ) {
-                val project = parameters.position.project
-                val service = project.getService(ExtensionServiceImpl::class.java)
-
-                // Call initialize here again, just in case
-                service.initialize()
-
-                val currentFile = parameters.originalFile.virtualFile
-                if (currentFile == null) return
-
-                val query = result.prefixMatcher.prefix.lowercase()
-                if (query.isEmpty()) return
-
-                val modules = service.getModulesForCompletion(currentFile, query)
-                for (module in modules) {
-                    result.addElement(
-                        LookupElementBuilder.create(module.moduleName)
-                            .withPresentableText(module.moduleName)
-                            .withTailText(" from ${module.importPath}")
-                            .withTypeText("namespace import")
-                            .withInsertHandler(InsertImportStatementHandler(module))
-                    )
-                }
-            }
-        })
+        extend(
+            CompletionType.BASIC,
+            PlatformPatterns.psiElement(),
+            ExtensionCompletionProvider(),
+        )
     }
 }
 
-class InsertImportStatementHandler(private val module: ModuleForCompletion) : InsertHandler<LookupElement> {
+class ExtensionCompletionProvider : CompletionProvider<CompletionParameters>() {
+    override fun addCompletions(
+        parameters: CompletionParameters,
+        context: ProcessingContext,
+        result: CompletionResultSet
+    ) {
+        val project = parameters.position.project
+        val service = project.getService(ExtensionServiceImpl::class.java)
+
+        // Call initialize here again, just in case
+        service.initialize()
+
+        val currentFile = parameters.originalFile.virtualFile
+        if (currentFile == null) return
+
+        val query = result.prefixMatcher.prefix.lowercase()
+        if (query.isEmpty()) return
+
+        val modules = service.getModulesForCompletion(currentFile, query)
+        for (module in modules) {
+            val importStatement = "import * as ${module.moduleName} from '${module.importPath}';\n"
+            if (!parameters.originalFile.fileDocument.text.contains(importStatement)) {
+                result.addElement(
+                    LookupElementBuilder.create(module.moduleName)
+                        .withPresentableText(module.moduleName)
+                        .withTailText(" from ${module.importPath}")
+                        .withTypeText("namespace import")
+                        .withInsertHandler(InsertImportStatementHandler(importStatement))
+                )
+            }
+        }
+    }
+}
+
+class InsertImportStatementHandler(private val importStatement: String) : InsertHandler<LookupElement> {
     override fun handleInsert(context: InsertionContext, item: LookupElement) {
         val document = context.document
         val project = context.project
@@ -54,13 +62,10 @@ class InsertImportStatementHandler(private val module: ModuleForCompletion) : In
         // The module name will already be automatically inserted.
         // We just need to add the import statement to the top of the file.
         WriteCommandAction.runWriteCommandAction(project, fun() {
-            val importStatement = "import * as ${module.moduleName} from '${module.importPath}';\n"
-
-            if (document.text.contains(importStatement)) return
-
-            document.insertString(0, importStatement)
-
-            PsiDocumentManager.getInstance(psiFile.project).commitDocument(document)
+            if (!document.text.contains(importStatement)) {
+                document.insertString(0, importStatement)
+                PsiDocumentManager.getInstance(psiFile.project).commitDocument(document)
+            }
         })
     }
 }
